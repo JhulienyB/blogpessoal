8cbfc84ae2d4a6da587f69f66119266291fe232f
JhulienyB <jhulienybucci@gmail.com>
2024-04-15 11:40:56 -0300

    Criação do projeto Blog Pessoal e criação de classes em relaçao a Postagem

---
+++ src/postagem/entities/postagem.entity.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, UpdateDateColumn, Column } from "typeorm";
+
+@Entity({name: "tb_postagem"})
+export class Postagem{
+
+    @PrimaryGeneratedColumn() //Define Chave Primária e Auto_Inclement
+    id: number;
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable: false})
+    titulo: string;
+
+    @IsNotEmpty()
+    @Column({length: 3000, nullable: false})
+    texto: string;
+    
+    @UpdateDateColumn()
+    data: Date;
+};
